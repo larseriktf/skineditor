aee340931bfcdc04cbea58288b8016eb318b33fa
larseriktf <lars.e.faber@gmail.com>
2021-12-27 01:35:31 +0100

    Added spring project

---
+++ skindojo/src/test/java/com/skineditor/skindojo/SkindojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skineditor.skindojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkindojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

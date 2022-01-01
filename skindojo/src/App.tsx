2a2bf91466debf5465aab232857fdc246d52fe58
larseriktf <lars.e.faber@gmail.com>
2022-01-01 22:22:20 +0100

    Added header menu bar and changed prettier configurations

--- skindojo/src/App.tsx
+++ skindojo/src/App.tsx
@@ -1,13 +1,9 @@
-import './style/App.css'
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Bruh
-        </p>
-      </header>
+      <Header />
     </div>
   )
 }
---
+++ skindojo/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="menu-bar">
+      <button className="menu-bar-button">File</button>
+      <button className="menu-bar-button">Settings</button>
+      <button className="menu-bar-button">Layout</button>
+    </header>
+  )
+}

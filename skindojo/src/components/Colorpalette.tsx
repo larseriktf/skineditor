7d39fbb7d02c8c8a566ad4c3552c29edccbadabc
larseriktf <lars.e.faber@gmail.com>
2022-01-28 21:25:15 +0100

    Refactored colorpallete to be leaner

--- skindojo/src/components/Colorpalette.tsx
+++ skindojo/src/components/Colorpalette.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { PaletteColor } from "./PaletteColor"
+import colorsBasic from "../res/colors_predefined.json"
 
 interface IProps {
   setColor: React.Dispatch<React.SetStateAction<string>>
@@ -9,78 +10,38 @@ export const Colorpalette = ({ setColor }: IProps) => {
   // States
 
   // Variables
-  const colorsPredefined = [
-    "#FFFFFF",
-    "#dfdfdf",
-    "#bfbfbf",
-    "#7f7f7f",
-    "#424242",
-    "#000000",
-    "#ffbec0",
-    "#ff7c82",
-    "#ff0023",
-    "#c5001a",
-    "#83000f",
-    "#440003",
-    "#ffdec2",
-    "#ffbc87",
-    "#ff7a31",
-    "#c53d1f",
-    "#834019",
-    "#63230d",
-    "#fffdc5",
-    "#fffc8e",
-    "#fffc50",
-    "#c1bc3c",
-    "#807d28",
-    "#434115",
-    "#bbfec4",
-    "#74fe8c",
-    "#00bd37",
-    "#007e25",
-    "#007e25",
-    "#004211",
-    "#b9fffe",
-    "#6efffe",
-    "#00fffe",
+  const colorsCustom = [
     "#00c0be",
     "#00807e",
     "#004342",
     "#bdc1fb",
-    "#7a85f8",
-    "#0032f6",
-    "#0025b8",
-    "#00197a",
-    "#000840",
-    "#ffc0fb",
-    "#ff82f9",
-    "#ff24f7",
-    "#c31bb9",
-    "#820f7b",
-    "#440440",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
+    "transparent",
   ]
-
-  const colorsUserDefined = ["#00c0be", "#00807e", "#004342", "#bdc1fb"]
-  let userDefinedPickers = 16
 
   return (
     <section className="palette">
       <p className="label">Basic Colors</p>
       <section className="palette-predefined">
-        {colorsPredefined.map((color) => (
-          <PaletteColor color={color} setColor={setColor} />
+        {colorsBasic.map((color, index) => (
+          <PaletteColor color={color} setColor={setColor} key={index} />
         ))}
       </section>
       <p className="label">Custom Colors</p>
       <section className="palette-user-defined">
-        {colorsUserDefined.map((color, index) => (
+        {colorsCustom.map((color, index) => (
           <PaletteColor color={color} key={index} setColor={setColor} />
         ))}
-        {[...Array(userDefinedPickers - colorsUserDefined.length)].map(
-          (e, index) => (
-            <PaletteColor color="transparent" key={index} setColor={setColor} />
-          )
-        )}
       </section>
     </section>
   )
